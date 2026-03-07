d8f0a984af42f6a5970ccca00b655aa98b53c7e8
CalvoJohnPaul <looselytiedshoelace@gmail.com>
2026-03-07 18:06:23 +0800

    fix: alert demoe

---
+++ website/src/components/Demo/Alert.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+import {Alert} from 'ui-ingredients';
+</script>
+
+<Alert.Root
+	class="mx-auto max-w-[24rem] gap-2.5 rounded border border-neutral-200 bg-white p-4 dark:border-neutral-700 dark:bg-neutral-700/25"
+>
+	<Alert.Title class="text-lg block font-semibold leading-none">
+		Title
+	</Alert.Title>
+	<Alert.Description
+		class="mt-1 text-sm block leading-none text-neutral-600 dark:text-neutral-400"
+	>
+		Description
+	</Alert.Description>
+</Alert.Root>
